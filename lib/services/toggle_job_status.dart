60a90f0e88d0150cf4f8a866ed0947180f00882b
Anant Raj <anantaditya6@gmail.com>
2021-02-23 10:56:13 +0530

    Minor bug fix

--- lib/services/toggle_job_status.dart
+++ lib/services/toggle_job_status.dart
@@ -2,9 +2,12 @@ import 'dart:convert';
 
 import 'package:job_admin_app/constants/apis.dart';
 import 'package:http/http.dart' as http;
+import 'package:job_admin_app/services/shared_preferences.dart';
 
 Future<bool> toggleJobStatus(String jobId,bool status) async {
   String url = BASE_API + TOGGLE_JOB_STATUS;
+  String jwt = await SharedPrefs().getUserJWTSharedPrefs();
+
   Map<String,String> map = {
     "job" : jobId,
     "active" : status.toString()
@@ -14,7 +17,8 @@ Future<bool> toggleJobStatus(String jobId,bool status) async {
       url,
       body: json.encode(map),
       headers: <String,String>{
-        "Content-type" : "application/json"
+        "Content-type" : "application/json",
+        "auth-token" : jwt
       }
   );
 
